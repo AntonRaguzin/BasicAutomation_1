0756f626999a788e18fdb57e652858234ca726f1
antonraguzin <raguzin82@gmail.com>
2022-08-02 17:24:54 +0300

    added tests

---
+++ src/test/java/ru/netology/cashBackHackService/CashBackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.cashBackHackService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CashBackHackServiceTest {
+
+    CashBackHackService service = new CashBackHackService();
+
+    @Test
+    public void shouldShowAdditionalPayment(){
+        int amount = 800;
+        int expected = 200;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+//    @Test
+//    public void shouldShowZeroAdditionalPayment(){
+//        int amount = 1_000;
+//        int expected = 0;
+//        int actual = service.remain(amount);
+//        assertEquals(expected, actual);
+//    }
+
+}
