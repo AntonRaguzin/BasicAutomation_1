271644613f71a4f4c4c4e9fd1f2fd8dbbcb3b445
antonraguzin <raguzin82@gmail.com>
2022-08-02 17:44:39 +0300

    added branch with tests on junit4

--- src/test/java/ru/netology/cashBackHackService/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/cashBackHackService/CashBackHackServiceTest.java
@@ -1,9 +1,10 @@
 package ru.netology.cashBackHackService;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 
-class CashBackHackServiceTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashBackHackServiceTest {
 
     CashBackHackService service = new CashBackHackService();
 
@@ -12,14 +13,14 @@ class CashBackHackServiceTest {
         int amount = 800;
         int expected = 200;
         int actual = service.remain(amount);
-        assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
     @Test
     public void shouldShowZeroAdditionalPayment(){
         int amount = 1_000;
         int expected = 0;
         int actual = service.remain(amount);
-        assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual);
     }
 
 }
